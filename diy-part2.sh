99f4b61eec05421e3241ed441b28e2421832cdb1
najloa <jecrdhs@gmail.com>
2025-07-01 20:13:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,22 +37,30 @@ sed -i '/encryption=sae-mixed/a \ \ \ \ set wireless.default_${dev}.key=blue1235
 # sed -i 's|local excluded_domain = {[^}]*}|local excluded_domain = {}|g' ./feeds/passwall/luci-app-passwall/root/usr/share/passwall/rule_update.lua
 # curl -s https://core.telegram.org/resources/cidr.txt > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/proxy_ip
 # : > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/chnlist
-# argon主题更新
+# argon主题
+find package -type d -name "*argon*"
+rm -rf ./feeds/luci/applications/luci-app-argon-config
+rm -rf ./feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 # git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon && rm -rf ./feeds/luci/themes/luci-theme-argon && \cp -rf ./package/luci-theme-argon ./feeds/luci/themes/
 # git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config && rm -rf ./feeds/luci/applications/luci-app-argon-config && \cp -rf ./package/luci-app-argon-config ./feeds/luci/applications/
 # argon主题
 # \cp -f $$GITHUB_WORKSPACE/argon/bg1.jpg ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 # 替换字体
+rm -f ./package/luci-theme-argon/htdocs/luci-static/argon/fonts/TypoGraphica*
+\cp -f $GITHUB_WORKSPACE/argon/fonts/* ./package/luci-theme-argon/htdocs/luci-static/argon/fonts
+\cp -f $GITHUB_WORKSPACE/argon/cascade.css ./package/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 # rm -f ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/fonts/TypoGraphica*
 # \cp -f $GITHUB_WORKSPACE/argon/fonts/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/fonts
 # \cp -f $GITHUB_WORKSPACE/argon/cascade.css ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 # 登录界面版本信息优化
-# sed -i 's/ ([<]%=\s*ver\.luciversion\s*%>)//g' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-# sed -i '/<%= ver\.distversion %>/d' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-# sed -i 's/ |$//' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i 's/ ([<]%=\s*ver\.luciversion\s*%>)//g' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i '/<%= ver\.distversion %>/d' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i 's/ |$//' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
 
-# grep "cbi-button-apply" ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
-# sed -i 's/<%:Log in%>/<%:Login%>/' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
-# sed -i 's|<%=media%>/img/argon\.svg||g' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
+# grep "cbi-button-apply" ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
+sed -i 's/<%:Log in%>/<%:Login%>/' ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
+sed -i 's|<%=media%>/img/argon\.svg||g' ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
 # golang
 # git clone --depth 1 https://github.com/immortalwrt/packages.git package/immortalwrt-packages && rm -rf ./feeds/packages/lang/golang && \cp -rf ./package/immortalwrt-packages/lang/golang ./feeds/packages/lang/
