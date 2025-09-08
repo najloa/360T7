8d9b037ff24ac000bd22e162a02a4faf88fe2118
najloa <jecrdhs@gmail.com>
2025-09-08 20:48:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,20 +19,20 @@ sed -i '/encryption=sae-mixed/a \ \ \ \ set wireless.default_${dev}.key=blue1235
 # curl -s https://core.telegram.org/resources/cidr.txt > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/proxy_ip
 # : > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/chnlist
 # argon主题
-#####find -type d -name "*argon*"
-#####git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon && rm -rf ./feeds/luci/themes/luci-theme-argon && \cp -rf ./package/luci-theme-argon ./feeds/luci/themes/ && rm -rf ./package/luci-theme-argon
-#####git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config && rm -rf ./feeds/luci/applications/luci-app-argon-config && \cp -rf ./package/luci-app-argon-config ./feeds/luci/applications/ && rm -rf ./package/luci-app-argon-config
+# find -type d -name "*argon*"
+# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon && rm -rf ./feeds/luci/themes/luci-theme-argon && \cp -rf ./package/luci-theme-argon ./feeds/luci/themes/ && rm -rf ./package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config && rm -rf ./feeds/luci/applications/luci-app-argon-config && \cp -rf ./package/luci-app-argon-config ./feeds/luci/applications/ && rm -rf ./package/luci-app-argon-config
 # 替换背景
 # \cp -f $$GITHUB_WORKSPACE/argon/bg1.jpg ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 # 替换字体
-#####rm -f ./package/luci-theme-argon/htdocs/luci-static/argon/fonts/TypoGraphica*
-#####\cp -f $GITHUB_WORKSPACE/argon/fonts/* ./package/luci-theme-argon/htdocs/luci-static/argon/fonts
-#####\cp -f $GITHUB_WORKSPACE/argon/cascade.css ./package/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+# rm -f ./package/luci-theme-argon/htdocs/luci-static/argon/fonts/TypoGraphica*
+# \cp -f $GITHUB_WORKSPACE/argon/fonts/* ./package/luci-theme-argon/htdocs/luci-static/argon/fonts
+# \cp -f $GITHUB_WORKSPACE/argon/cascade.css ./package/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 # 登录界面版本信息优化
-#####sed -i 's/ ([<]%=\s*ver\.luciversion\s*%>)//g' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-#####sed -i '/<%= ver\.distversion %>/d' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-#####sed -i 's/ |$//' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-#####sed -i 's/<%:Log in%>/<%:Login%>/' ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
-#####sed -i 's|<%=media%>/img/argon\.svg||g' ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
+# sed -i 's/ ([<]%=\s*ver\.luciversion\s*%>)//g' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+# sed -i '/<%= ver\.distversion %>/d' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+# sed -i 's/ |$//' ./package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+# sed -i 's/<%:Log in%>/<%:Login%>/' ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
+# sed -i 's|<%=media%>/img/argon\.svg||g' ./package/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
 # golang
-#git clone --depth 1 https://github.com/immortalwrt/packages.git package/immortalwrt-packages && rm -rf ./feeds/packages/lang/golang && \cp -rf ./package/immortalwrt-packages/lang/golang ./feeds/packages/lang/
+# git clone --depth 1 https://github.com/immortalwrt/packages.git package/immortalwrt-packages && rm -rf ./feeds/packages/lang/golang && \cp -rf ./package/immortalwrt-packages/lang/golang ./feeds/packages/lang/
