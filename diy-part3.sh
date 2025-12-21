2f8acd860cadae6a14e93d5cd3e805ab27b30f84
najloa <jecrdhs@gmail.com>
2025-12-21 12:48:55 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -7,7 +7,7 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-sed -i 's/192.168.1.1/192.168.10.1/' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.10.1/' ./package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='OpenWrt'/" ./package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt-2.4G/NW/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/NW/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
