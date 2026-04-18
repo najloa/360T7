d778706d14c34317db5d009ef8aa8f68e1537dd6
najloa <jecrdhs@gmail.com>
2026-04-18 15:08:48 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -25,5 +25,53 @@ sed -i 's|local excluded_domain = {[^}]*}|local excluded_domain = {}|g' ./packag
 curl -s https://core.telegram.org/resources/cidr.txt > ./package/passwall-luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_ip
 
 # argon主题
+rm -rf ./feeds/luci/themes/luci-theme-argon && git clone --depth 1 https://github.com/immortalwrt/luci tmp_luci && \cp -rf tmp_luci/themes/luci-theme-argon/ feeds/luci/themes/luci-theme-argon/ && rm -rf tmp_luci
 # 替换默认背景
 \cp -f $GITHUB_WORKSPACE/argon/bg1.jpg ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# 替换字体
+rm -f ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/fonts/TypoGraphica*
+\cp -f $GITHUB_WORKSPACE/argon/fonts/* ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/fonts
+# 修改界面
+# 多彩效果
+sed -i '/@keyframes anim-fade-in/i\
+@keyframes shine {\
+  0% { background-position: -200% center; }\
+  100% { background-position: 200% center; }\
+}\
+' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+# 主页标识
+sed -i '/\.main-left \.sidenav-header \.brand {/,/}/c\
+.main-left .sidenav-header .brand {\
+  display: block;\
+  margin: 0;\
+  font-size: 1.8rem;\
+  font-family: "TypoGraphica";\
+  text-decoration: none;\
+  text-align: center;\
+  cursor: default;\
+  background: linear-gradient(\
+    120deg,\
+    #00fff7,\
+    #007cf0,\
+    #ff4ecd,\
+    #00fff7\
+    );\
+  background-size: 300% 300%;\
+  -webkit-background-clip: text;\
+  -webkit-text-fill-color: transparent;\
+  animation: shine 5s linear infinite;\
+}' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+
+# 登录页标识
+# 居中
+sed -i 's|.login-page .login-container .login-form .brand{display:flex;-webkit-box-align:center;align-items:center;margin:50px auto 100px 50px;color:#525461;color:var(--default);justify-content:center}|.login-page .login-container .login-form .brand{display:flex;align-items:center;justify-content:center;margin:50px auto 100px auto;color:var(--default);}|g' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+# 删除图标
+sed -i 's|.login-page .login-container .login-form .brand .icon{width:50px;height:auto;margin-right:25px}||g' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+
+sed -i 's|.login-page .login-container .login-form .brand .brand-text{font-size:1.25rem;font-weight:700;font-family:"TypoGraphica"}|.login-page .login-container .login-form .brand .brand-text{margin-right:0px;font-size:2.6rem;font-weight:400;font-family:"TypoGraphica",sans-serif;word-break:break-word;background:linear-gradient(120deg, #00fff7, #007cf0, #ff4ecd, #00fff7);background-size:300% 300%;-webkit-background-clip:text;-webkit-text-fill-color:transparent;animation:shine 5s linear infinite;}|g' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+
+sed -i 's|.login-page .login-container .login-form .cbi-button-apply{width:100% !important;box-shadow:rgba(0,0,0,0.1) 0 0 50px 0;font-weight:600;font-size:15px;color:#fff;color:var(--white);text-align:center;width:100%;cursor:pointer;min-height:50px;background-color:#5e72e4 !important;background-color:var(--primary) !important;border-radius:6px;outline:none;border-width:initial;border-style:none;border-color:initial;border-image:initial;padding:10px 0px;margin:30px 0px 100px;transition:all .3s !important;letter-spacing:.8rem}.login-page .login-container .login-form .cbi-button-apply:hover,|.login-page .login-container .login-form .cbi-button-apply{width:100% !important;min-height:45px;margin:30px 0 100px;padding:10px 0;font-size:15px;font-weight:600;text-align:center;letter-spacing:.35rem;background:rgba(0,0,0,0);backdrop-filter:blur(8px);border:none;border-radius:9999px;outline:none;cursor:pointer;transition:all 0.25s ease;position:relative}.login-page .login-container .login-form .cbi-button-apply:hover{box-shadow:0 0 0 2px rgba(255,255,255,0.5)}|g' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+# 登录界面底部
+sed -i '/a:active {/,/}/ s/var(--primary)/#dddddd/g' ./feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+sed -i '/<footer/,/<\/footer>/ { /<a class="luci-link"/d }' ./feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
+sed -i 's|<%=media%>/img/argon\.svg||g' ./feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/sysauth.ut
