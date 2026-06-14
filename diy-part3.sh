b863f6fda4d1123397963f21dcfb454d345433ad
najloa <jecrdhs@gmail.com>
2026-06-14 12:37:53 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/192.168.6.1/192.168.10.1/' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/' ./package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt-2.4G/NW/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/NW/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/36/auto/' ./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
