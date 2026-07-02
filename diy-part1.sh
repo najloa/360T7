c61e0c61cc902e6df0e5d0c532bd9b0a4aed164e
najloa <jecrdhs@gmail.com>
2026-07-03 01:03:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+printf '%s\n' \
+"src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" \
+"src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" \
+| cat - feeds.conf.default > tmp && mv tmp feeds.conf.default
+echo "===== feeds.conf.default 内容如下 ====="
+cat feeds.conf.default
